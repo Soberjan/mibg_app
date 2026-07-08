855bd8ad15b9a7902ab611f11dc4ba9523552764
Soberjan <operations.rannsoknir@gmail.com>
2026-07-09 00:41:36 +0300

    debugging hell on android

--- src/static/scripts/lobby/lobbySockets.js
+++ src/static/scripts/lobby/lobbySockets.js
@@ -30,7 +30,7 @@ export function otherPlayerJoinedSocket(res) {
 
 export function playerRegisteredSocket(res) {
     const registerPlayerText = document.getElementById("registerPlayerText");
-    registerPlayerText.textContent = "вы словили сокет что кто-то зарегался ептыть");
+    registerPlayerText.textContent = "вы словили сокет что кто-то зарегался ептыть";
     console.log(res);
     console.log(state);
     state.players[res.player_id].status = "registered";
